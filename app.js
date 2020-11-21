f3845868e9253d508caacb0063f30087f4f64a58
MELI CANTAMUTTO <MELICANTAMUTTO@gmail.com>
2020-11-21 13:12:01 -0300

    ejercicio js

---
+++ app.js
@@ -0,0 +1,8 @@
+console.log('Cantidad de caracteres')
+
+alert('Calculador de cantidad de caracteres')
+
+const cadena = prompt('Ingrese el texto a calcular');
+const caracteres = cadena.length;
+
+alert(`El texto ingresado tiene ${caracteres} caracteres`)
